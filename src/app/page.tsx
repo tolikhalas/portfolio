f9db585ad7c9c9104780d7d303ae378c7f362c53
Anatolii Halas <tolikgalas@gmail.com>
2024-06-14 20:39:02 +0300

    FE-2-Components/ Added blank <Hero /> component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="bg-black-100 relative mx-auto flex flex-col items-center justify-center overflow-hidden px-5 sm:px-10">
+      <div className="w-full max-w-7xl">
+        <Hero />
+      </div>
+    </main>
+  );
 }
