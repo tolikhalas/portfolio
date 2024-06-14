6b35cb63313db26af7a1a2bfffaa3ec84b12f81d
Anatolii Halas <tolikgalas@gmail.com>
2024-06-14 21:38:11 +0300

    FE-2-Components/ Added Spotlights

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="pb-20 pt-36">
+      <Spotlight
+        className="-left-10 -top-40 h-screen md:-left-32 md:-top-20"
+        fill="white"
+      />
+      <Spotlight className="left-full top-10 h-[80vh] w-[50vw]" fill="purple" />
+      <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
