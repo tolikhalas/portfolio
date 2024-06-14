f4413fb2325da74450aa0eddd2c7587b8f4a16fa
Anatolii Halas <tolikgalas@gmail.com>
2024-06-14 23:44:34 +0300

    FE-2-Components/ Added Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,44 @@
-import React from "react";
-import { Spotlight } from "./ui/Spotlight";
+import { FaLocationArrow } from "react-icons/fa6";
+
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import SpotlightList from "./ui/SpotlightList";
+import GridBackround from "./ui/GridBackround";
 
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-      <Spotlight
-        className="-left-10 -top-40 h-screen md:-left-32 md:-top-20"
-        fill="white"
-      />
-      <Spotlight className="left-full top-10 h-[80vh] w-[50vw]" fill="purple" />
-      <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      <SpotlightList />
+      <GridBackround />
+      <div className="relative z-10 my-20 flex justify-center">
+        <div className="flex max-w-[89vw] flex-col items-center justify-center md:max-w-2xl lg:max-w-[60vw]">
+          <p className="max-w-80 text-center text-xs uppercase tracking-widest text-blue-100">
+            Dynamic Web Magic with Next.js
+          </p>
+
+          {/**
+           *  Link: https://ui.aceternity.com/components/text-generate-effect
+           *
+           *  change md:text-6xl, add more responsive code
+           */}
+          <TextGenerateEffect
+            words="Transforming Concepts into Seamless User Experiences"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+
+          <p className="mb-4 text-center text-sm md:text-lg md:tracking-wider lg:text-2xl">
+            Hi! I&apos;m Tolik, a Full Stack Developer based in Ukraine.
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
