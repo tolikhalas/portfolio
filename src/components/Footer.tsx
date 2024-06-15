fe10c2f446d426bf334818f5b870be56d271ff7f
Anatolii Halas <tolikgalas@gmail.com>
2024-06-15 21:56:20 +0300

    FE-2-Components/ Change Footer info

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,13 +5,13 @@ import MagicButton from "./MagicButton";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
+    <footer className="w-full pb-10 pt-20" id="contact">
       {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+      <div className="absolute -bottom-72 left-0 min-h-96 w-full">
         <img
           src="/footer-grid.svg"
           alt="grid"
-          className="w-full h-full opacity-50 "
+          className="h-full w-full opacity-50"
         />
       </div>
 
@@ -20,7 +20,7 @@ const Footer = () => {
           Ready to take <span className="text-purple">your</span> digital
           presence to the next level?
         </h1>
-        <p className="text-white-200 md:mt-10 my-5 text-center">
+        <p className="my-5 text-center text-white-200 md:mt-10">
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
@@ -32,16 +32,16 @@ const Footer = () => {
           />
         </a>
       </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Adrian Hajdin
+      <div className="mt-16 flex flex-col items-center justify-between md:flex-row">
+        <p className="text-sm font-light md:text-base md:font-normal">
+          Copyright © {new Date().getFullYear()} Anatolii Halas
         </p>
 
-        <div className="flex items-center md:gap-3 gap-6">
+        <div className="flex items-center gap-6 md:gap-3">
           {socialMedia.map((info) => (
             <div
               key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              className="saturate-180 flex h-10 w-10 cursor-pointer items-center justify-center rounded-lg border border-black-300 bg-black-200 bg-opacity-75 backdrop-blur-lg backdrop-filter"
             >
               <img src={info.img} alt="icons" width={20} height={20} />
             </div>
